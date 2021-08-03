d1bba2344fe7fe5c12effb34f1069456613865f1
PastelWhale <aviate8@gmail.com>
2021-08-03 22:49:14 +0900

    Add Searching For Elements

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,4 +1,27 @@
-document.title = "Oh good";
-document.location;
-const title = document.getElementById("title");
-console.dir("title");
+//Id로 접근
+const all = document.getElementById("all");
+console.log(all);
+
+//ClassName으로 접근
+const hellos = document.getElementsByClassName("hellos");
+console.log(hellos);
+
+//TagName으로 접근, array로 반환해준다.
+const title = document.getElementsByTagName("h1");
+console.log(title);
+
+//queryselector, CSS처럼 요소를 가져올 수 있다,
+//단 하나의 element를 리턴한다. 여러 개 있을 경우 첫번째 것을 가져온다.
+const shot = document.querySelector(".hello h1:first-child");
+console.log(shot);
+
+const rabbit = document.querySelector("#all");
+// const rabbit = document.getElementById("all");
+console.log(rabbit);
+
+//여러 개의 요소를 가져 온다.
+const tiger = document.querySelectorAll(".hello h1");
+console.log(tiger);
+
+const eagle = document.querySelector(".eagle h1");
+eagle.innerText = "eagle";
