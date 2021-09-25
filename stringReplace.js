36f45255fc1b6fe803e236dc2c51f4ed2e68273b
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-09-26 08:50:59 +0900

    Add: string replace

---
+++ stringReplace.js
@@ -0,0 +1,7 @@
+var str =
+  "https://raw.githubusercontent.com/JaeseungJung/Sol/main/toy/Tree-Item/{id}.json";
+var newstr = str.replace(
+  "{id}",
+  "0000000000000000000000000000000000000000000000000000000000001000"
+);
+console.log(newstr);
